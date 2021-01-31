24d2aea937cc7261d1b6fa30af8f1c83f37c92cc
Yuliia Dvoinos <69351064+YuliiaDvoinos@users.noreply.github.com>
2021-01-31 09:12:59 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -2,3 +2,11 @@
 // input#name - input(событие input), подставляет его текущее значение в span#name
 // - output.Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
 
+const nameInputRef = document.querySelector('#name-input');
+const nameOutputref = document.querySelector('#name-output');
+
+nameInputRef.addEventListener('input', event => {
+    nameOutputref.textContent = event.target.value;
+    if (nameInputRef.value == '')
+        nameOutputref.textContent = 'незнакомец';
+});
